10d5bc08631f564578744a8e18810a79ad8393f8
Aditya Pandey <adityapandey3082004@gmail.com>
2024-07-31 00:16:14 +0530

    Graphql types, resolvers and queries

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,20 +1,21 @@
 import express from "express";
 import {ApolloServer} from "@apollo/server";
 import {expressMiddleware} from "@apollo/server/express4"
-
+import { User } from "./user";
 
 export async function initServer(){
     const app=express()
 
     const server = new ApolloServer({
         typeDefs:`
+            ${User.types}
             type Query {
-                sayHello: String
+                ${User.queries}
             }
         `,
         resolvers:{
             Query:{
-                sayHello:()=>"Hello from GraphQL server"
+               ...User.resolvers.queries,
             },
          },
       });
---
+++ src/app/user/index.ts
@@ -0,0 +1,6 @@
+import { types} from "./types";
+// import { } from "./mutations";
+import {queries } from "./queries";
+import { resolvers} from "./resolvers"
+
+export const User={ types, queries,resolvers}
