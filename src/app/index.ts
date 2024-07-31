6a63596e62982583b3371915655054fc1afc06c5
Aditya Pandey <adityapandey3082004@gmail.com>
2024-08-01 00:35:01 +0530

    cors updated

--- src/app/index.ts
+++ src/app/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import {ApolloServer} from "@apollo/server";
 import {expressMiddleware} from "@apollo/server/express4"
 import { User } from "./user";
+import cors from "cors";
 
 export async function initServer(){
     const app=express()
@@ -22,6 +23,7 @@ export async function initServer(){
 
       await server.start();
       app.use(express.json());
+      app.use(cors());
       app.use("/graphql",expressMiddleware(server))
 
       return app;
